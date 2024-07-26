f2222dc05c56f2a22eb3f7c2d1affca2fc6098dd
Radu05 <campianradu05@gmail.com>
2024-07-26 13:47:40 +0300

    Merge branch 'main' of https://github.com/Radu05/Pandas_tryouts

---
+++ Main_File.py
@@ -0,0 +1,103 @@
+'''
+This is a test
+'''
+
+import	pandas as pd
+import fnmatch
+
+base_partlist_name= 'partlist1.csv'
+
+changes_list_name= 'changes1.csv'
+
+def get_sheetframe_info(partlist): # Get the sheetframe 'Component ID' for further use.
+
+	index_sheetframe= partlist.loc[partlist['Symbol Name'] == 'din_a1_v_sdsym_2.smb'].index
+
+	sheetframe_info1= partlist.take(index_sheetframe, axis= 0)
+
+	index_concat= partlist.loc[partlist['Symbol Name'] == 'din_a1_v_sdsym_1.smb'].index
+
+	sheetframe_info2= partlist.take(index_concat, axis= 0)
+
+	sheetframe_info1= pd.concat([sheetframe_info1, sheetframe_info2])
+
+	return(sheetframe_info1)
+
+def import_partlist_csv(base_partlist): #Import the base partlist
+		
+	# Make a back up of this file so that it will not be overwrittern 
+	backup_partlist= base_partlist.copy()
+	
+	# Clean the file by:
+	
+	# remove useless items
+	remove_useless_nets(backup_partlist)
+	remove_sheetframe_symbols(backup_partlist)
+	remove_useless_comp(backup_partlist)
+
+	# remove duplicate RefDese
+	backup_partlist.drop_duplicates(subset= ['Reference Designator'], keep= 'first', inplace= True, ignore_index= True)
+	
+	return(backup_partlist)
+
+def remove_sheetframe_symbols(partlist): #Remove Sheetframe symbols din_a1_v_sdsym_*.smb
+
+	remove_symbols= ['din_a1_v_sdsym_2.smb','din_a1_v_sdsym_1.smb']
+
+	for cases in remove_symbols:
+
+		filtered= fnmatch.filter(partlist['Symbol Name'], cases)
+
+		for x in filtered:
+		
+			index_remove= partlist.loc[partlist['Symbol Name'] == x].index
+
+			partlist.drop(index= index_remove, inplace= True)
+	
+	return(partlist)
+
+def remove_useless_nets(partlist): # Remove Net related components (sheetconnectors /gnds /voltage rails)
+
+	remove_useless_nets= ['shcon*.smb','nc.smb','gndd*.smb','volt*.smb','export*.smb']
+
+	for cases in remove_useless_nets:
+
+		filtered= fnmatch.filter(partlist['Symbol Name'], cases)
+
+		for x in filtered:
+		
+			index_remove= partlist.loc[partlist['Symbol Name'] == x].index
+
+			partlist.drop(index= index_remove, inplace= True)
+	
+	return(partlist)
+
+def remove_useless_comp(partlist): # ! No EMPTY CELLS ALLOWED, remove useless components such as: HB*, KR*, P*, STP*, LP*, MH*
+
+	remove_useless_comp= ['HB*', 'KR*', 'P*', 'STP*', 'LP*', 'MH*']
+
+	for cases in remove_useless_comp:
+
+		filtered = fnmatch.filter(partlist['Reference Designator'], cases)
+
+		for x in filtered:
+
+			index_remove= partlist.loc[partlist['Reference Designator'] == x].index
+
+			partlist.drop(index= index_remove, inplace= True)
+
+	return(partlist)
+	
+# Get the base_partlist from a csv file
+base_partlist_input= pd.read_csv(base_partlist_name)
+
+backup_partlist_input= import_partlist_csv(base_partlist_input)
+
+backup_partlist_input.to_csv('backup_partlist_input.csv', index=False)
+
+# Get the sheetframe symbol information
+sheetframe_info_input= get_sheetframe_info(base_partlist_input)
+
+sheetframe_info_input.to_csv('sheetframe_info_input.csv', index=False)
+
+print('Done')
---
+++ test2.py
@@ -0,0 +1,10 @@
+import string
+
+# Generate the alphabet in uppercase
+alphabet_upper = string.ascii_uppercase
+
+for x in alphabet_upper:
+
+    x= x + '*' + ','
+
+print(x)
